35ac79aa378a0087effc899247822ecb90fac098
Suhas Garla <Suhas.G@phamax.ch>
2022-12-20 15:37:20 +0530

    sequelize config and product model

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,8 @@ let express = require('express');
 let logger = require('morgan');
 const cors=require('cors');
 
+const {sequelize} = require('./models'); // import models
+
 let bodyParser = require('body-parser')
 let authentication = require('./routes/authentication');
 let billing = require('./routes/billing');
@@ -22,4 +24,8 @@ app.use('/billing',billing);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, async() =>{
+    console.log('server running on http://localhost:5000');
+    //await sequelize.sync({force: true}); //This creates the table, dropping them first if they already existed
+    await sequelize.authenticate();
+});
--- Backend/routes/billing.js
+++ Backend/routes/billing.js
@@ -1,9 +1,27 @@
-let express = require('express');
+const express = require('express');
+var Product = require('../models/index').Product
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-   console.log(req.body.data)
-   res.status(200).json({ message: ' Bill Details Saved' })
-   });
-   
-module.exports=router;
+
+
+
+router.post("/item", async (req, res) => {
+   const {product_name,price,quantity,total_price} = req.body
+   const productName= product_name
+
+
+    const totalPrice=total_price
+
+   try {
+      const item = await Product.create({ productName,price,quantity,totalPrice });
+      return res.json(item);
+   } catch (err) {
+      return res.status(500).json(err);
+   }
+});
+
+// router.post('/', async (req, res) => {
+//    console.log(req.body.data.items)
+//    res.status(200).json({ message: ' Bill Details Saved' })
+// });
+module.exports = router;
